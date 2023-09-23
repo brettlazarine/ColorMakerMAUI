aaffada3778bb6568d47e151d1799f96ee33eb16
Brett Lazarine <jblazarine@gmail.com>
2023-09-23 13:15:53 -0500

    app completed

--- ColorMaker/ColorMaker/MainPage.xaml.cs
+++ ColorMaker/ColorMaker/MainPage.xaml.cs
@@ -1,24 +1,68 @@
-﻿namespace ColorMaker;
+﻿using System.Diagnostics;
+using CommunityToolkit.Maui.Alerts;
+
+namespace ColorMaker;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	bool isRandom;
+	string hexValue;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+    private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        if (!isRandom)
+        {
+        var red = sldRed.Value;
+		var green = sldGreen.Value;
+		var blue = sldBlue.Value;
+
+		var color = Color.FromRgb(red, green, blue);
+
+		SetColor(color);
+        }
+    }
+
+	private void SetColor(Color color)
 	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		Debug.WriteLine(color.ToString());
+		btnRandom.BackgroundColor = color;
+		Container.BackgroundColor = color;
+		hexValue = color.ToHex();
+		lblHex.Text = hexValue;
 	}
+
+    private void btnRandom_Clicked(object sender, EventArgs e)
+    {
+		isRandom = true;
+		var random = new Random();
+
+		var color = Color.FromRgb(
+			random.Next(0, 256), 
+			random.Next(0, 256), 
+			random.Next(0, 256));
+
+		SetColor(color);
+
+		sldRed.Value = color.Red;
+		sldGreen.Value = color.Green;
+		sldBlue.Value = color.Blue;
+
+		isRandom = false;
+    }
+
+    private async void ImageButton_Clicked(object sender, EventArgs e)
+    {
+		await Clipboard.SetTextAsync(hexValue);
+
+		var toast = Toast.Make(
+			"Color copied",
+			CommunityToolkit.Maui.Core.ToastDuration.Short,
+			12);
+    }
 }
 
